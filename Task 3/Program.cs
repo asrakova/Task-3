d602fdd6f6e6e668fc3c9ac0f11c4b0a57ee2e43
Анна <@LAPTOP-63K8I5TL>
2017-08-12 20:16:06 +0500

    Полное решение. Без проверки ввода

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -10,12 +10,20 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            // Считываем значение x
+            Console.WriteLine("Введите x");
             double x = double.Parse(Console.ReadLine());
+            // Считываем значение y
+            Console.WriteLine("Введите y");
             double y = double.Parse(Console.ReadLine());
-            double u = 0;
+            double u = 0;       // Результат
 
-            if (x * x + (y - 1) * (y - 1) <= 1 && y <= 1 - x * x) u = x - y;
+            // Вычисляем значение u в зависимости от принадлежности точки зашрихованной области
+            if (x * x + (y - 1) * (y - 1) <= 1 && y <= 1 - x * x)
+                u = x - y;
             else u = x * y + 7;
+
+            // Выводим результат
             Console.WriteLine(u);
             Console.ReadLine();
         }
